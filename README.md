1348ae0e406e07822d8536eabf2a6f6d952b4eba
JonasCodeworks <codeworks@artyfakt.org>
2024-11-06 19:36:05 +0100

    Initial setup

---
+++ README.md
@@ -0,0 +1,8 @@
+# Pagemark
+A chrome extension to bookmark specific portions webpages
+
+## Build
+Running "npm run build" will bundle everything and put it in the "dist" folder
+
+## Install
+On the chrome extension page, activate "Developer mode" and use the "Load unpacked" option to load the dist folder
---
+++ rollup.config.mjs
@@ -0,0 +1,18 @@
+// rollup.config.mjs
+import resolve from "@rollup/plugin-node-resolve"
+import commonjs from "@rollup/plugin-commonjs"
+import { chromeExtension, simpleReloader } from "rollup-plugin-chrome-extension"
+
+export default {
+	input: "src/manifest.json",
+	output: {
+		dir: 'dist',
+		format: 'esm'
+	},
+  plugins: [
+		chromeExtension(),
+		simpleReloader(),
+    resolve(),
+    commonjs()
+  ]
+}
